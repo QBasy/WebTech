27e250bdf273b14d1285d5e5c35fc80e5e819924
QoidynBasy <c0mplexie1@gmail.com>
2023-11-06 15:53:47 +0600

    not first commit

--- js/feedback.js
+++ js/feedback.js
@@ -16,27 +16,7 @@ const newDiv = $("<div class=\"container card\" style=\"width:500px\">\n" +
     "</div>");
 
 const submittedDiv = $("<h3 class='text-center'>Thanks for your Feedback, here is a button to start a simple game</h3>" +
-    "<button onclick='startGame()' id=\"startGame\" type=\"submit\" class=\"btn btn-primary\">Start Game</button>")
-
-const gameDiv = $(`
-        <div class="container">
-            <h1>Match the Countries with Their Capitals</h1>
-            
-            <div class="category" id="category1">
-                    <div class="item" id="item1" draggable="true" data-match="target1">Python</div>
-                <div class="item" id="item2" draggable="true" data-match="target2">Java</div>
-                <div class="item" id="item3" draggable="true" data-match="target3">C#</div>
-            </div>
-
-            <div class="category" id="category2">
-                <div class="item" id="target1" data-match="item1">Django</div>
-                <div class="item" id="target2" data-match="item2">Spring</div>
-                <div class="item" id="target3" data-match="item3">.NET</div>
-            </div>
-            
-            <p id="result"></p>
-        </div>
-        `);
+    "<button onclick='startGame()' id=\"startGame\" type=\"submit\" class=\"btn btn-primary\">Start Game</button>");
 
 function createFeedback() {
     $("#Feedback").replaceWith(newDiv);
@@ -45,45 +25,28 @@ function createFeedback() {
     });
 }
 
-function checkEmailMsg(email, msg) {
-    if (feedback.name.test(email)) {
-        return feedback.message.test(msg);
-    } else {
-        return false;
-    }
+function checkEmailMsg(email) {
+    return feedback.name.test(email);
 }
 
 function submitFeedback() {
-    $(newDiv).replaceWith(submittedDiv);
+    if (!checkEmailMsg(document.querySelector('input').value)) {
+        alert("Email, or Message is/are incorrect")
+    } else {
+        const audio = new Audio("../audio/audio1.mp3");
+        audio.play();
+        $(newDiv).replaceWith(submittedDiv);
+    }
 }
 
-function startGame() {
-    const gameContainer = $("#gameContainer");
-        gameContainer.html(gameDiv);
-
-    $(".item").on("dragstart", function (e) {
-        e.originalEvent.dataTransfer.setData("text", e.target.id);
-    });
-
-    function checkMatch() {
-        let correct = 0;
-        $(".item").each(function () {
-            const matchId = $(this).data("match");
-            if ($(this).parent().attr("id") === "category2" && $(this).attr("id") === matchId) {
-                correct++;
-            }
-        });
-
-        if (correct === $(".item").length) {
-            $("#result").text("Congratulations! You matched all the countries with their capitals!");
-        }
-    }
-
-    $("#category2").on("drop", function (e) {
-        e.preventDefault();
-        const data = e.originalEvent.dataTransfer.getData("text");
-        e.target.appendChild(document.getElementById(data));
-        e.target.style.backgroundColor = "#99FF99";
-        checkMatch();
-    });
+function allowDrop(event) {
+    event.preventDefault();
+}
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
 }
